64f74bfd69c59d4bba1c573d44e261dd240dcb1e
mahkaila <mahkailaemmajones@gmail.com>
2016-03-30 00:59:00 +1300

    6

--- geolocation/script.js
+++ geolocation/script.js
@@ -1,34 +1,30 @@
 window.onload = function() {
+
 	// Check to see if the browser supports the GeoLocation API.
 	if (navigator.geolocation) {
-		// Get the location
+	
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var lat = position.coords.latitude;
 			var lon = position.coords.longitude;
-			// Show the map
 			showMap(lat, lon);
 		});
+
 	} else {
- 	 // Print out a message to the user.
+  // Print out a message to the user.
   	document.write('Your browser does not support GeoLocation');
 	}
+	
+}
 
-
-// Show the user's position on a Google map.
 function showMap(lat, lon) {
 	// Create a LatLng object with the GPS coordinates.
 	var myLatLng = new google.maps.LatLng(lat, lon);
-	// Create the Map Options
+	
 	var mapOptions = {
- 		zoom: 8,
-  		center: myLatLng,
-  		mapTypeId: google.maps.MapTypeId.ROADMAP
+  	zoom: 8,
+  	center: myLatLng,
+  	mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	// Add a Marker to the Map
-	var marker = new google.maps.Marker({
-  		position: myLatLng,
-  		map: map,
-  		title: 'Found you!'
-	});
 }
-}
+
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
